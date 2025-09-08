8491b850f2bed1287f778c1fd9c534e9f2a142be
Alex Agafonov <agafonov.alexander@gmail.com>
2025-09-08 21:53:49 +0300

    Доработка Лончскрина и создание вьюхи основного экрана

---
+++ TravelSchedule/Views/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  TravelSchedule
+//
+//  Created by Alexander Agafonov on 07.09.2025.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    MainView()
+}
