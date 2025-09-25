3b551a8f9569014d1e7aeca7355c82e51d15a726
Alex Agafonov <agafonov.alexander@gmail.com>
2025-09-25 23:15:40 +0300

    Экран и модель перевозчика

---
+++ TravelSchedule/Views/CarrierInfoViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  CarrierInfoViewModel.swift
+//  TravelSchedule
+//
+//  Вью модель перевозчика
+
+import SwiftUI
+import Combine
+
+final class CarrierInfoViewModel: ObservableObject {
+    @Published private(set) var carrier: Carrier
+    @Published var title: String = "Информация о перевозчике"
+
+    init(carrier: Carrier) {
+        self.carrier = carrier
+    }
+
+    func reloadFromNetwork() {
+        // TODO: доработать
+    }
+}
