af5a3c83da8f829b21b03776cf40c2254617e333
Alex Agafonov <agafonov.alexander@gmail.com>
2025-09-28 10:05:48 +0300

    Правка модели перевозчиков

--- TravelSchedule/Views/Carrier/CarriersListView.swift
+++ TravelSchedule/Views/Carrier/CarriersListView.swift
@@ -13,63 +13,63 @@ final class CarriersListViewModel: ObservableObject {
             dateText: "14 января", depart: "06:15", arrive: "12:05",
             durationText: "5 ч 50 м",
             transferNote: nil,
-            email: nil,
-            phoneE164: nil,
-            phoneDisplay: nil
+            email: "info@rzd.ru",
+            phoneE164: "+78007750000",
+            phoneDisplay: "8 800 775-00-00"
         ),
         CarrierOption(
             carrierName: "ФГК", logoName: "fgk_logo",
             dateText: "15 января", depart: "01:15", arrive: "09:00",
             durationText: "7 ч 45 м",
             transferNote: "С пересадкой в Твери",
-            email: nil,
-            phoneE164: nil,
-            phoneDisplay: nil
+            email: "info@railfgk.ru",
+            phoneE164: "+78002504777",
+            phoneDisplay: "8-800-250-4777"
         ),
         CarrierOption(
             carrierName: "Урал логистика", logoName: "ural_logo",
             dateText: "16 января", depart: "12:30", arrive: "21:00",
             durationText: "8 ч 30 м",
             transferNote: nil,
-            email: nil,
-            phoneE164: nil,
-            phoneDisplay: nil
+            email: "general@ulgroup.ru",
+            phoneE164: "+74957838383",
+            phoneDisplay: "+7 (495) 783-83-83"
         ),
         CarrierOption(
             carrierName: "РЖД", logoName: "rzd_logo",
             dateText: "17 января", depart: "22:30", arrive: "08:15",
             durationText: "9 ч 45 м",
             transferNote: "С пересадкой в Костроме",
-            email: nil,
-            phoneE164: nil,
-            phoneDisplay: nil
+            email: "info@rzd.ru",
+            phoneE164: "+78007750000",
+            phoneDisplay: "8 800 775-00-00"
         ),
         CarrierOption(
             carrierName: "РЖД", logoName: "rzd_logo",
             dateText: "17 января", depart: "18:05", arrive: "23:15",
             durationText: "5 ч 10 м",
             transferNote: nil,
-            email: nil,
-            phoneE164: nil,
-            phoneDisplay: nil
+            email: "info@rzd.ru",
+            phoneE164: "+78007750000",
+            phoneDisplay: "8 800 775-00-00"
         ),
         CarrierOption(
             carrierName: "РЖД", logoName: "rzd_logo",
             dateText: "17 января", depart: "18:05", arrive: "23:15",
             durationText: "5 ч 10 м",
             transferNote: nil,
-            email: nil,
-            phoneE164: nil,
-            phoneDisplay: nil
+            email: "info@rzd.ru",
+            phoneE164: "+78007750000",
+            phoneDisplay: "8 800 775-00-00"
         ),
         CarrierOption(
             carrierName: "Урал логистика", logoName: "ural_logo",
             dateText: "16 января", depart: "12:30", arrive: "21:00",
             durationText: "8 ч 30 м",
             transferNote: nil,
-            email: nil,
-            phoneE164: nil,
-            phoneDisplay: nil
+            email: "general@ulgroup.ru",
+            phoneE164: "+74957838383",
+            phoneDisplay: "+7 (495) 783-83-83"
         )
     ]
 }
@@ -221,12 +221,9 @@ struct CarriersListView: View {
             id: option.logoName,
             name: option.carrierName,
             logoAsset: option.logoName,
-            // email: nil,
-            // phoneE164: nil,
-            // phoneDisplay: nil
-            email: "info@carrier.ru",
-            phoneE164: "+79041234567",
-            phoneDisplay: "+7 (904) 123-45-67"
+            email: option.email,
+            phoneE164: option.phoneE164,
+            phoneDisplay: option.phoneDisplay
         )
     }
 }
