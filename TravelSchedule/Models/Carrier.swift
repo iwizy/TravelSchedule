b7c2e6fcd80b20a88124c0eb46f7cf08b557a44a
Alex Agafonov <agafonov.alexander@gmail.com>
2025-09-29 23:35:41 +0300

    вынос мока перевозчика

--- TravelSchedule/Models/Carrier.swift
+++ TravelSchedule/Models/Carrier.swift
@@ -4,7 +4,7 @@
 //
 //  Модель перевозчика
 
-import Foundation
+
 
 struct Carrier: Identifiable, Hashable, Codable {
     let id: String
@@ -13,15 +13,4 @@ struct Carrier: Identifiable, Hashable, Codable {
     let email: String?
     let phoneE164: String?
     let phoneDisplay: String?
-}
-
-extension Carrier {
-    static let mock = Carrier(
-        id: "rzd",
-        name: "ОАО «РЖД»",
-        logoAsset: "rzd_logo",
-        email: "i.lozgkina@yandex.ru",
-        phoneE164: "+79043292771",
-        phoneDisplay: "+7 (904) 329-27-71"
-    )
 }
